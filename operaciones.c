f6ee0d0d628274c1685e515520478b08d3e61901
Leonardo Fiorenzo <lfiorenzo@frba.utn.edu.ar>
2026-05-04 17:42:45 -0300

    Atiende review: explicacion de la funcion multiplicar

--- operaciones.c
+++ operaciones.c
@@ -14,6 +14,7 @@ int restar(int a, int b) {
 
 /* ── multiplicar — implementar en feature/mi-funcion ─────────────────────── */
 //Pasa dos variables enteras y las devuelve multiplicadas.
+//Esta funcion recibe dos enteros y devuelve el producto de los mismos.
 int multiplicar(int a, int b) {
     return a * b; 
 }
